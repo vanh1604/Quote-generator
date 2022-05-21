a5e8f8ce1841d39002a56964e44597ece07ae376
vanh1604 <VietAnhScout@gmail.com>
2022-05-21 17:17:36 +0700

    first commit

---
+++ quote.js
@@ -0,0 +1,83 @@
+// Ref to elm
+const quoteContainer = document.getElementById("container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+const endContainer = document.getElementById("end-container");
+
+let quotes = [
+  `Em ơi... <i class="far fa-kiss-wink-heart"></i>`,
+  "Em có biết gì không???",
+  "Anh thíchh...",
+  `Đi lượn cùng em <i class="fas fa-motorcycle"></i>`,
+  `Đi ăn cùng em <i class="fas fa-utensils"></i>`,
+  `Mình cùng nhau có một đứa nhé <i class="fas fa-baby"></i>`,
+  `Và cuối cùng là...`,
+  `Anh yêu em rất nhiều`,
+];
+let i = 0;
+// Call APi
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const res = await fetch(apiUrl);
+    quotes = await res.json();
+    newQuote();
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Make new quote
+function newQuote() {
+  if (quotes.length === 0) return;
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  // Set Author
+  quote.author = quote.author ? quote.author : "Unknown";
+  authorText.textContent = quote.author;
+
+  // Handle too leng quote
+  quote.text = quote.text ? quote.text : "Unknown";
+  if (quote.text.length >= 50) quoteText.classList.add("long-quote");
+  else quoteText.classList.remove("long-quote");
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+function getSent() {
+  quoteText.innerHTML = quotes[i++];
+  if (i >= quotes.length) {
+    newQuoteBtn.classList.add("hide");
+    endContainer.classList.remove("hide");
+  }
+}
+
+// loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+newQuoteBtn.addEventListener("click", () => {
+  quoteContainer.classList.remove("fade-in");
+  quoteContainer.classList.add("fade-out");
+  setTimeout(() => {
+    getSent();
+    quoteContainer.classList.add("fade-in");
+    quoteContainer.classList.remove("fade-out");
+  }, 460);
+});
+// set quote,hide loader
+
+
+// On load
+getSent();
